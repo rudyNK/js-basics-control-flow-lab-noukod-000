4e2bdde7b73180899e769b40d1b2fd98b094cf70
Rudy Ravelin <rudy@noukod.com>
2019-02-26 17:51:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  let response;
+
+   if (distance <= 400) {
+    response = "This one is on me!"
+
+  }
+
+  else if (distance > 2000) {
+    if (distance > 2500) {
+      response = "No can do."
+    }
+
+     else {
+      response = "I will gladly take your thirty bucks."
+    }
+  };
+
+   return response;
+};
+
+ function ternaryCheckCity(city) {
+  const response = city === 'NYC' ? "Ok, sounds good." : "No go."
+
+   return response;
+};
+
+ function switchOnCharmFromTip(tip) {
+  let response;
+
+   switch(tip) {
+  case 'generous':
+    response = "Thank you so much."
+    break;
+  case 'not as generous':
+    response = "Thank you."
+    break;
+  default:
+    response = 'Bye.'
+  };
+
+   return response;
+};
